dfd6a81ba36d46c05f498cce14f5d1918123c72d
Kovalchuk2008 <artemkovalcuk49@gmail.com>
2025-10-02 10:20:14 +0300

    Добавьте файлы проекта.

---
+++ 02.29.25/main.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Elevator
+{
+	int topFloor;
+	int groundFloor;
+	bool OnOff;
+	bool works;
+	int position;
+public:
+	Elevator(int ground, int top);
+	void onOff();
+	void setPosition(int); 
+	int GetPosition();
+	void Call();
+};
+Elevator::Elevator(int ground, int top)
+{
+	groundFloor = ground;
+	topFloor = top;
+	OnOff = false; 
+	works = false;
+	position = 0;
+}
+void Elevator::onOff()
+{
+	OnOff = !OnOff;
+}
+void Elevator::setPosition(int position)
+{
+	this->position = position;
+}
+int Elevator::GetPosition()
+{
+	return position;
+}
+void Elevator::Call()
+{
+	cout << "Enter your floor from: " << groundFloor << " to: " << topFloor << endl;
+	cin >> position;
+	int floor;
+	cout << "Enter your current position:  = " << position << endl;
+	if (!OnOff)
+	{
+		cout << "Elevator on: ";
+		return;
+	}
+	if (floor < groundFloor && floor > topFloor)
+	{
+		cout << "There is no floor: ";
+	}
+	else
+	{
+		position = floor;
+		cout << "Elevator going on floor" << position << endl;
+	}
+}
+
+int main()
+{
+	setlocale(0, "ru");
+	Elevator el(1, 10);
+	el.onOff();
+	el.Call();
+}
+
